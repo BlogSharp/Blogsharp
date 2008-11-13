dea5365bf0f6da2d176f89b0f5e0bfe8dd072aee
Tuna Toksoz <tehlike@gmail.com>
2008-11-13 22:02:31 +0000

    Adding ApplicationEvents

---
+++ BlogSharp.Core/Event/ApplicationEvents/ApplicationStartingEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlogSharp.Core.Event.ApplicationEvents
+{
+	public class ApplicationStartingEvent:AbstractEvent<IApplication>,ICancellableEvent<IApplication>
+	{
+		public ApplicationStartingEvent(IApplication app):base(app)
+		{
+			
+		}
+
+
+		#region ICancellableEvent<IApplication> Members
+
+		public bool Cancel
+		{
+			get; set;
+		}
+
+		#endregion
+	}
+}
