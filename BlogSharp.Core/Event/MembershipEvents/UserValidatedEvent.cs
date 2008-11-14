995fd055972e21616b7bde8c3dcb52fc19d6ae80
Tuna Toksoz <tehlike@gmail.com>
2008-11-14 06:27:02 +0000

    Introducing more events

---
+++ BlogSharp.Core/Event/MembershipEvents/UserValidatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlogSharp.Core.Services.Post;
+
+namespace BlogSharp.Core.Event.MembershipEvents
+{
+	public class UserValidatedEvent:AbstractEvent<IPostService>
+	{
+		public UserValidatedEvent(IPostService postService):base(postService)
+		{
+			
+		}
+	}
+}
---
+++ BlogSharp.Core/Event/MembershipEvents/UserValidatingEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlogSharp.Core.Services.Post;
+
+namespace BlogSharp.Core.Event.MembershipEvents
+{
+	public class UserValidatingEvent:AbstractEvent<IPostService>,ICancellableEvent
+	{
+		public UserValidatingEvent(IPostService postService):base(postService)
+		{
+			
+		}
+
+		public string Username { get; set; }
+
+
+		#region ICancellableEvent Members
+
+		public bool Cancel
+		{ 
+			get;
+			set;
+		}
+
+		#endregion
+	}
+}
