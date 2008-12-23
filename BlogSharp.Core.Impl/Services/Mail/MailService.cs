7d1caed8f3f28fe86e2cb6d44e43b7f1f8874c4c
Tuna Toksoz <tehlike@gmail.com>
2008-12-23 16:51:51 +0000

    MailService implementation, without tests.

--- BlogSharp.Core.Impl/Services/Mail/MailService.cs
+++ BlogSharp.Core.Impl/Services/Mail/MailService.cs
@@ -9,17 +9,41 @@ namespace BlogSharp.Core.Impl.Services.Mail
 {
 	public class MailService:IMailService
 	{
-		public MailService(string defaultFromAddress,string defaultDisplayName)
+		public MailService(string defaultFromAddress,string defaultDisplayName,string host,int port)
 		{
 			this.defaultFromAddress = defaultFromAddress;
 			this.defaultDisplayName = defaultDisplayName;
+			this.smtpClient = new SmtpClient(host, port);
+
 		}
 
+		private readonly SmtpClient smtpClient;
 		private readonly string defaultDisplayName;
 		private readonly string defaultFromAddress;
 		public void Send(MailAddress to, MailAddress from, MailAddress replyto, string subject, string message)
 		{
-			throw new NotImplementedException();
+			MailMessage m=new MailMessage(from,to);
+			m.ReplyTo = replyto;
+			m.Subject = subject;
+			m.Body = message;
+			smtpClient.Send(m);
 		}
+
+		#region IMailService Members
+
+
+		public void Send(MailAddress to, MailAddress replyto, string subject, string message)
+		{
+			this.Send(to, replyto, new MailAddress(this.defaultFromAddress, this.defaultDisplayName),
+			          subject, message);
+		}
+
+		public void Send(MailAddress to, string subject, string message)
+		{
+			this.Send(to,null, new MailAddress(this.defaultFromAddress, this.defaultDisplayName),
+			          subject, message);
+		}
+
+		#endregion
 	}
 }
--- BlogSharp.Core/Services/Mail/IMailService.cs
+++ BlogSharp.Core/Services/Mail/IMailService.cs
@@ -5,5 +5,7 @@ namespace BlogSharp.Core.Services.Mail
 	public interface IMailService
 	{
 		void Send(MailAddress to, MailAddress from, MailAddress replyto, string subject, string message);
+		void Send(MailAddress to, MailAddress replyto, string subject, string message);
+		void Send(MailAddress to, string subject, string message);
 	}
 }
